2e3cc7c22bb421a7eb20044392353d48f8f2ab48
Daniil <Daniilsemen@gmail.com>
2024-03-03 11:55:10 +0200

    init: project

---
+++ src/config.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+/** Project's main configuration header
+ *  Should be included via CMakeLists.txt:`target_precompile_headers()`
+ *	**VSCode**
+ *		Intellisense hint option should be present in `.vscode/settings.json`
+ *	    Othervise manually add work-/userspace setting:
+ *  	`"C_Cpp.default.forcedInclude": ["${workspaceFolder}/src/config.hpp"]`
+ */
+
+#ifndef BPLAN_CONFIG_PCH
+#define BPLAN_CONFIG_PCH // `config.hpp` presence marking
+#endif
+
+#define IMGUI_DEFINE_MATH_OPERATORS // Enable operator overloads for ImGui (like: `ImVec2`)
+
+#include <stdint.h>
+using ushort = unsigned short;
+using ulong  = unsigned long long;
+using uchar  = unsigned char;
+using uint   = unsigned int;
